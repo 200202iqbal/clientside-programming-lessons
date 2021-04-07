9c90e176260021a266ae5a53d94f751c5867f74c
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-04-07 13:09:34 +0900

    lessons 2
    lessons 2

--- LESSONS 2/js/variable.js
+++ LESSONS 2/js/variable.js
@@ -20,4 +20,51 @@ console.log("Price = " + price);
 console.log("Amount = " + amount);
 console.log("Tax rate = " + tax_rate);
 console.log("Total price = " + total_price);
+var hp = 3;
+console.log(hp+1);
 
+hp = 1;
+console.log(hp-2);
+
+hp=5;
+console.log(hp*3);
+
+hp= 4;
+console.log(hp/5);
+
+hp = 6;
+console.log(hp%5);
+
+hp = 5;
+++hp;
+console.log(hp);
+
+hp = 5;
+--hp;
+console.log(hp);
+
+hp = 10;
+hp += 5;
+console.log(hp);
+
+hp = 10;
+hp -= 5;
+console.log(hp);
+
+hp = 10;
+hp *= 5;
+console.log(hp);
+
+hp = 10;
+hp /= 5;
+console.log(hp);
+hp = 24;
+hp %= 5;
+console.log(hp);
+
+hp = 10;
+var status_message = monster + " のHPは " + hp;
+console.log(status_message);
+
+var calculate_message = "お会計は" + total_price +"円です・";
+console.log(calculate_message);
